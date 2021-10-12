e3e7da1ef172b0c7a232f6fb81896d04a3914a50
Rica <desiree2009@att.net>
2021-10-12 16:51:28 -0400

    centering
    progress centering each category.

---
+++ firstJs.js
@@ -0,0 +1,18 @@
+let person ={
+    name: "Mosh",
+    age: 10
+}; 
+
+let selectedColors =['red', "blue","yellow"]//creating an array
+
+console.log(selectedColors[1]);//printing an element in an array
+
+
+selectedColors[0]="lala" //reassigning the 0 element in the array
+
+
+function greet (name){
+    console.log("HI");
+}
+
+greet("Erica");
