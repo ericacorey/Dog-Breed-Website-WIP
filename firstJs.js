d6b39026c769c9098994fe84319e8fbb5d6e8257
Rica <desiree2009@att.net>
2021-12-25 23:24:05 -0500

    powerpoint and pattern
    placeholder powerpoint with stripe pattern added !

--- firstJs.js
+++ firstJs.js
@@ -1,18 +1,28 @@
-let person ={
-    name: "Mosh",
-    age: 10
-}; 
+var slideIndex = 1;
+showSlides(slideIndex);
 
-let selectedColors =['red', "blue","yellow"]//creating an array
-
-console.log(selectedColors[1]);//printing an element in an array
-
-
-selectedColors[0]="lala" //reassigning the 0 element in the array
-
-
-function greet (name){
-    console.log("HI");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-greet("Erica");
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
